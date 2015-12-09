85d455ba308f8859bcd68c7125cb7d01cd0de3dd
Mark Oosterveld <mark@oosterveld.org>
2015-12-09 00:40:13 -0500

    experimented with router. made some progress on firebase auth

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ This is my Meet-Up Event Planner, for the UDACITY Course,
 [Building High Conversion Web Forms][1], part of the 
 [Senior Web Developer Nanodegree program][2]
 
+You can see it in action here: [https://m13d-meetme.firebaseapp.com/][7]
+
 I am using [Polymer][3], and borowing heavily from the [Polymer Start Kit][4]
+and [PolymerLabs todo-list][5], as described in
+[End to End with Polymer (The Polymer Summit 2015)][6]
 
 [1]: https://www.udacity.com/course/building-high-conversion-web-forms--ud890
 [2]: https://www.udacity.com/course/senior-web-developer--nd802
 [3]: https://www.polymer-project.org/1.0/
 [4]: https://developers.google.com/web/tools/polymer-starter-kit/
+[5]: https://github.com/PolymerLabs/todo-list
+[6]: https://www.youtube.com/watch?v=1f_Tj_JnStA
+[7]: https://m13d-meetme.firebaseapp.com/
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,34 @@
 var gulp = require('gulp');
+var debug = require('gulp-debug');
 var ghPages = require('gulp-gh-pages');
+var size = require('gulp-size');
+var vulcanize = require('gulp-vulcanize');
  
+var DIST = 'dist';
+var dist = function(subpath) {
+    return !subpath ? DIST : path.join(DIST, subpath);
+};
+
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());
+});
+
+gulp.task('vulcanize', function() {
+    return gulp.src('app/index.html')
+        .pipe(debug())
+        .pipe(size({
+            showFIles: true,
+            title: 'src'
+        }))
+        .pipe(vulcanize({
+            stripComments: true,
+            inlineCss: true,
+            inlineScripts: true
+        }))
+        .pipe(size({
+            showFIles: true,
+            title: 'dest'
+        }))
+        .pipe(gulp.dest('dist'));
 });
