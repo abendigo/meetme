8bf35e3039ab69efe56ec51fe6d001884d5278ae
Mark Oosterveld <marko@oanda.com>
2016-03-04 12:00:03 -0500

    fix layout issues

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,7 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var size = require('gulp-size');
 var vulcanize = require('gulp-vulcanize');
-var crisper = require('gulp-crisper');
-var gif = require('gulp-if');
-var babel = require('gulp-babel');
-var sourcemaps = require('gulp-sourcemaps');
- 
+
 var DIST = 'dist';
 var dist = function(subpath) {
     return !subpath ? DIST : path.join(DIST, subpath);
@@ -20,7 +16,7 @@ gulp.task('deploy', function() {
 gulp.task('vulcanize', function() {
     return gulp.src('app/index.html')
         .pipe(size({
-            showFIles: true,
+            showFiles: true,
             title: 'src'
         }))
         .pipe(vulcanize({
@@ -32,41 +28,5 @@ gulp.task('vulcanize', function() {
             showFiles: true,
             title: 'dest'
         }))
-        .pipe(gulp.dest('dist'));
-});
-
-
-gulp.task('marko', function() {
-    return gulp.src('app/index.html')
-        // .pipe(sourcemaps.init({debug: true}))
-        .pipe(size({
-            showFIles: true,
-            title: 'src'
-        }))
-        .pipe(vulcanize({
-            stripComments: true,
-            inlineCss: true,
-            inlineScripts: false
-        }))
-        .pipe(size({
-            showFiles: true,
-            title: 'vulcanized'
-        }))
-        .pipe(crisper({
-            scriptInHead: false,
-            onlySplit: false
-        }))
-        .pipe(size({
-            showFiles: true,
-            title: 'crisp'
-        }))
-        // .pipe(gif('*.js', babel({
-        //     presets: ['es2015']
-        // })))
-        // .pipe(size({
-        //     showFiles: true,
-        //     title: 'babel'
-        // }))
-        // .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist'));
 });
